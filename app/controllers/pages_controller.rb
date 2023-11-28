1165235279d2d9f6d08f4f9ad37f2c7566e1a5ab
quentinkhoo <quentin.khoo@ascendaloyalty.com>
2023-11-28 15:23:35 +0800

    implement arbitrary file read vuln

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,20 @@
+require 'base64'
+
 class PagesController < ApplicationController
   # index is a public page
   def index
+    image = params.fetch(:img, 'anya.png')
+    file = File.open(image)
+    @img = Base64.encode64(file.read)
+    file.close
   end
 
-  # anya is a private page, only anya can enter
+  # anya is a private page, only anya user can enter
   def anya
-    if current_user != 'anya'
+    if current_user.blank?
       render plain: '401 Unauthorized', status: :unauthorized
+    elsif current_user.name != 'anya'
+      render plain: '401 You are not anya', status: :unauthorized
     end
   end
 end
