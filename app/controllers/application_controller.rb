7ec026ec1cdb53925c3eca50fc0c7fcc5904329a
quentinkhoo <quentin.khoo@ascendaloyalty.com>
2023-11-30 04:49:13 +0800

    filter path traversal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :is_anya
 
   def current_user
     # If session[:user_id] is nil, set it to nil, otherwise find the user by id.
     @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+  end
+
+  def is_anya
+    if current_user && current_user.name == 'anya'
+      @is_anya = true
+    end
   end
 
   def not_found_method
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,25 @@ class PagesController < ApplicationController
   
   # index is a public page
   def index
-    anya_path = Rails.application.config.anya_dir
-    image = anya_path + params.fetch(:img, 'anya.png')
-    file = File.open(image)
-    @img = Base64.encode64(file.read)
-    file.close
-    
-    @anyas = []
-    anya_pattern = anya_path + '*.txt'
-    Dir.glob(anya_pattern) do |filename|
-      anya = File.open(filename)
-      @anyas.append(anya.read)
-      anya.close
+    begin
+      anya_path = Rails.application.config.anya_dir
+      image = (anya_path + params.fetch(:img, 'anya.png')).gsub("../", "")
+      file = File.open(image)
+      @img = Base64.encode64(file.read)
+      file.close
+      
+      @anyas = []
+      anya_pattern = anya_path + '*.txt'
+      Dir.glob(anya_pattern) do |filename|
+        anya = File.open(filename)
+        @anyas.append(anya.read)
+        anya.close
+      end
+    rescue => e
+      image = anya_path + 'anya.png'
+      file = File.open(image)
+      @img = Base64.encode64(file.read)
+      file.close
     end
   end
 
